47d2672f05d709113aa02d8f3976326583d980cb
haider-nayyar <haidernayyar@gmail.com>
2019-01-13 03:44:49 +0500

    chnages in SearchController with wildcard fixed

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -10,7 +10,7 @@ class SearchController extends Controller
 {
     function index($term){
         $courses = Course::where('course_name', 'like','%'. $term . '%')->get();
-        $users = User::where('firstName','like','%',$term . '%')->orWhere('lastName','like',$term . '%')->get();
+        $users = User::where('firstName','like','%'.$term . '%')->orWhere('lastName','like',$term . '%')->get();
         return response()->json([$courses,$users]);
     }
 }
