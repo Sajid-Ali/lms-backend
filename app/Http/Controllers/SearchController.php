8e4dbff651ff6096292d84c1d02dae4b5e653992
haider-nayyar <haidernayyar@gmail.com>
2019-01-13 03:42:47 +0500

    chnages in SearchController with wildcard

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -9,8 +9,8 @@ use Illuminate\Http\Request;
 class SearchController extends Controller
 {
     function index($term){
-        $courses = Course::where('course_name', 'like', $term . '%')->get();
-        $users = User::where('firstName','like',$term . '%')->orWhere('lastName','like',$term . '%')->get();
+        $courses = Course::where('course_name', 'like','%'. $term . '%')->get();
+        $users = User::where('firstName','like','%',$term . '%')->orWhere('lastName','like',$term . '%')->get();
         return response()->json([$courses,$users]);
     }
 }
